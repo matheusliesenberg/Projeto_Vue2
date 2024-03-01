456c77b1c57b68e72d29677e0b39bcc161872999
matheusliesenberg <matheusliesenberg3@gmail.com>
2024-03-01 14:44:44 -0300

    Implementação da tabela de 1

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,53 @@
 <script setup>
-    const nome = 'Matheus'
-    const idade = 16
+const nome = 'Matheus Liesenberg'
+const idade = 16
 
-    function inverter(texto) {
-        return texto.split('').reverse().join('')
-    }
+function inverter(texto) {
+  return texto.split('').reverse().join('').toUpperCase()
+}
 
-    function saudacao() {
-        return `Olá, ${nome}!`
-    }
+function saudacao() {
+  return `Olá, ${nome}!`
+}
+import { ref } from 'vue';
+
+const contador = ref(0)
+function descrementar() {
+  if (contador.value > 0) {
+    contador.value--;
+  }
+}
+function acrementar() {
+  contador.value++;
+}
+function resetar() {
+  contador.value = 0;
+}
 </script>
 
 <template>
-    <div class="info">
-        <h1>Exemplo 1 - Mostrando informações na tela</h1>
-        <p>Nome: {{ nome }} </p>
-        <p>Idade: {{ idade }}  anos</p>
-        <hr />
-        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
-        <p>O nome tem {{ nome.length }}  caracteres</p>
-        <p>O nome invertido é: {{ inverter(nome) }} </p>
-        <hr />
-        <p> Exemplo de saudação usando função</p>
-        <p> {{ saudacao() }}  </p>
-    </div>
+  <div class="info">
+    <h1>Exemplo 1 - Mostrando informações na tela</h1>
+    <p>Nome: {{ nome }} </p>
+    <p>Idade: {{ idade }} anos</p>
+    <hr />
+    <p>Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
+    <p>O nome tem {{ nome.length }} caracteres</p>
+    <p>O nome invertido é: {{ inverter(nome) }}</p>
+    <hr />
+    <p> Exemplo de saudação usando função</p>
+    <p> {{ saudacao() }} </p>
+  </div>
+  <button @click="resetar">Resetar</button>
+  <button @click="acrementar">+</button>
+  <button @click="descrementar">-</button>
+  
+  <p>valor do contator: {{ contador }}</p>
 </template>
 
 <style scoped>
 .info {
-  background-color: rgba(35,12,22,1);
+  background-color: rgba(35, 12, 22, 1);
   padding: 20px 30px;
   color: rgb(183, 210, 219);
   border-radius: 10px;
@@ -38,5 +57,13 @@ h1 {
   color: white;
   font-weight: bold;
   font-size: 1.5em;
+}
+
+button {
+  background-color: rgba(35, 12, 22, 1);
+  color: rgb(255, 255, 255);
+  font-weight: bolder;
+  border: 0;
+  margin: 2px;
 }
 </style>
